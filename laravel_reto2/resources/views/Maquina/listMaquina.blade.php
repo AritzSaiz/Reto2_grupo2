6b02cb72ba9b858394f0bbe15deb36afcbf4381c
JulenCorpas2004 <julen.corpas@ikasle.egibide.org>
2025-01-20 10:45:53 +0100

    boton añadir colocado

--- laravel_reto2/resources/views/Maquina/listMaquina.blade.php
+++ laravel_reto2/resources/views/Maquina/listMaquina.blade.php
@@ -92,8 +92,8 @@
         </div>
 
 
-        <div class="d-flex justify-content-center flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Lista de maquinas</h1>
+        <div class="d-flex justify-content-around flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1>Lista de maquinas</h1>
             <div class="btn-toolbar align-items-right mb-2 mb-md-0">
                 <a type="button" href="{{ route('maquina.create') }}" class="btn btn-sm btn-outline-secondary">
                     <span data-feather="plus-circle"></span>
--- laravel_reto2/resources/views/Seccion/listSeccion.blade.php
+++ laravel_reto2/resources/views/Seccion/listSeccion.blade.php
@@ -98,7 +98,7 @@
         </div>
 
 
-        <div class="d-flex justify-content-center flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <div class="d-flex justify-content-around flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1>Lista de Secciones </h1>
             <div class="btn-toolbar align-items-right mb-2 mb-md-0">
                 <a type="button" href="{{ route('seccion.create') }}" class="btn btn-sm btn-outline-secondary">
