8375ce493ac65da4acd473fc834273c453d9c126
aritz <aritz.saiz@ikasle.egibide.org>
2025-01-23 17:08:41 +0100

    cambios en la mantenimientos preventivos

--- laravel_reto2/database/migrations/2025_01_20_082956_create_mantenimiento_maquinas_table.php
+++ laravel_reto2/database/migrations/2025_01_20_082956_create_mantenimiento_maquinas_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('mantenimientos_maquinas', function (Blueprint $table) {
             $table->unsignedBigInteger('mantenimiento_id');
             $table->unsignedBigInteger('maquina_id');
-            /* TODO : ¿Más atributos? */
             $table->timestamps();
             $table->softDeletes();
 
