a76ea4194a34b61c1d5950fed7e7e352f96b3035
Lorena <lorena.arbe@ikasle.egibide.org>
2025-01-24 10:50:45 +0100

    Implementar la mitad de los filtros. Los filtros de Estado, Campus, Sección y Categoría funcionan tanto individualmente como combinados entre sí.

--- laravel_reto2/database/seeders/SeccionesSeeder.php
+++ laravel_reto2/database/seeders/SeccionesSeeder.php
@@ -14,10 +14,10 @@ class SeccionesSeeder extends Seeder
     {
         DB::table('secciones')->insert([
             ['codigo' => '5010', 'nombre' => 'Taller Mecánico 1  010', 'campus_id' => 1, 'created_at' => now(), 'updated_at' => now()],
-            ['codigo' => '5011', 'nombre' => 'Taller Aurrera XXI', 'campus_id' => 2, 'created_at' => now(), 'updated_at' => now()],
-            ['codigo' => '5012', 'nombre' => 'Taller Mecánico 2  012', 'campus_id' => 3, 'created_at' => now(), 'updated_at' => now()],
-            ['codigo' => '5014', 'nombre' => 'Taller Control Numérico 014', 'campus_id' => 4, 'created_at' => now(), 'updated_at' => now()],
-            ['codigo' => '5017', 'nombre' => 'Taller Mecánico 3  017', 'campus_id' => 5, 'created_at' => now(), 'updated_at' => now()],
+            ['codigo' => '5011', 'nombre' => 'Taller Aurrera XXI', 'campus_id' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['codigo' => '5012', 'nombre' => 'Taller Mecánico 2  012', 'campus_id' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['codigo' => '5014', 'nombre' => 'Taller Control Numérico 014', 'campus_id' => 2, 'created_at' => now(), 'updated_at' => now()],
+            ['codigo' => '5017', 'nombre' => 'Taller Mecánico 3  017', 'campus_id' => 3, 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
